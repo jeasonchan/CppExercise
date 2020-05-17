d5385328f8779625d0861736360dd2b57ef08ce1
jeasonchan <439994526@qq.com>
2020-05-17 15:26:36 +0800

    update

--- CppNewStarter/main.cpp
+++ CppNewStarter/main.cpp
@@ -1,8 +1,11 @@
-#include <iostream>
+#include <iostream> //使用标准库的做法是，用尖括号从标准类库引用头文件，再通过std命名空间使用
+
 #include "bean/person.h"
-#include "netfw.h"
-#include "string.h"
-#include "strings.h"
+#include "bean/student.h"
+//使用自定义的头文件，使用双引号从项目的根目录下以相对路径引用；
+// cpp风格的标准做法还是自定义一个命名空间，再通过命名空间导出变量名再使用
+
+using namespace std;
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
@@ -14,6 +17,7 @@ int main() {
 
     std::cout << person.getAge() << person.getName();
 
+    Student::allPersonJump();
 
 
 
