e4d13f51b05aee1e40e4d302f43ac9b606e9853e
jeasonchan <439994526@qq.com>
2020-10-03 22:51:11 +0800

    to web

--- CppNewStarter/test_2D_array/Main.cpp
+++ CppNewStarter/test_2D_array/Main.cpp
@@ -1,6 +1,31 @@
 //
 // Created by chenr on 2020/10/2.
 //
+#include <iostream>
+
+void createFunction1(unsigned int n) {
+    unsint i, j;
+    const int b = 123;
+
+    //elements in array is int[123] type
+    //st like int[b] *array2D=new (int[123])[n]
+    int (*array2D)[b] = new int[n][b];
+    for (int height = 0; height < n; ++height) {
+        for (int width = 0; width < b; ++width) {
+            std::cout << array2D[height][width] << " ";
+        }
+    }
+
+    delete[] array2D;
+}
+
+
+void createFunction2(unsigned int height, unsigned int width) {
+    //
+    auto array2D = new int *[height];
+
+
+}
 
 int main() {
     int a[2][3] = {1, 2, 3};
