4679fb7941f670e3fdbff83436e01bd0a48e42ea
jeasonchan <439994526@qq.com>
2020-12-18 00:16:56 +0800

    complete leetcode714

--- CppNewStarter/leetcode714/Main.cpp
+++ CppNewStarter/leetcode714/Main.cpp
@@ -21,8 +21,18 @@ namespace leetcode714 {
             int days = static_cast<int>(prices.size());
             vector<vector<int>> dp{2, vector<int>(days)};
 
+            //第0天，手上没有股票时的最大收益
+            dp[0][0] = 0;
 
-            return 0;
+            //第0天，手上有股票时的最大收益
+            dp[1][0] = -prices[0];
+
+            for (int i = 1; i < prices.size(); ++i) {
+                dp[0][i] = max(dp[0][i - 1], dp[1][i - 1] + prices[i] - fee);
+                dp[1][i] = max(dp[1][i - 1], dp[0][i - 1] - prices[i]);
+            }
+
+            return dp[0][days - 1];
         }
     };
 
@@ -30,10 +40,10 @@ namespace leetcode714 {
 
 
 int main(int argc, char *argv[]) {
-    vector<int> vec{1, 2, 35, 4, 6, 54, 9, 67, 57, 57, 0};
+    vector<int> vec{1, 3, 2, 8, 4, 9};
 
     leetcode714::Solution solution;
-    solution.maxProfit(vec, 0);
+    solution.maxProfit(vec, 2);
 
     return 0;
 }
