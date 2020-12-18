18a76145aaa98623c3e421c98306d44823b4c2bc
jeasonchan <439994526@qq.com>
2020-12-18 17:31:09 +0800

    Create Main.cpp
    增加leetcode738

---
+++ CppNewStarter/leetcode738/Main.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+#include <iostream>
+
+
+namespace leetcode738 {
+    using namespace std;
+
+    class Solution {
+    public:
+        int monotoneIncreasingDigits(int N) const {
+            vector<unsigned short> record{};
+
+            int bitNum = 0;
+            for (int i = 0; N > 0; ++i) {
+                record[i] = N % 10;
+                N = N - record[i];
+                bitNum = i;
+            }
+
+            int ret = 0;
+            for (int i = bitNum; i >= 0; --i) {
+
+
+            }
+
+
+
+
+            return ret;
+
+        }
+    };
+
+
+}
+
+
+int main(int argc, char *argv[]) {
+
+
+    return 0;
+}
