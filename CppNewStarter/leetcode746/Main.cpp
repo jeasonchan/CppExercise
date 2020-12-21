fa95e03d5c5e72253f96fc6020d876de53968740
jeasonchan <439994526@qq.com>
2020-12-21 23:56:24 +0800

    leetcode

---
+++ CppNewStarter/leetcode746/Main.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <iostream>
+
+
+namespace leetcode746 {
+    using namespace std;
+
+
+    class Solution {
+    public:
+        int minCostClimbingStairs(vector<int> &cost) {
+            //插入一个作为最高点
+            cost.push_back(0);
+
+
+
+
+
+
+
+
+            return 0;
+        }
+    };
+
+}
+
+
+int main(int argc, char *argv[]) {
+
+
+    return 0;
+};
