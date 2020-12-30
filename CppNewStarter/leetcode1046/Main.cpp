cb29ad16b5ca769d54b55aff8588a978bc7186ee
jeasonchan <439994526@qq.com>
2020-12-30 23:56:26 +0800

    leetcode1046

--- CppNewStarter/leetcode1046/Main.cpp
+++ CppNewStarter/leetcode1046/Main.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <queue>
+#include <algorithm>
 
 namespace leetcode1046 {
     using namespace std;
@@ -10,7 +11,35 @@ namespace leetcode1046 {
             auto less = [](int &o1, int &o2) -> bool { return o1 < o2; };
 
             //其实可以直接用std中仿函数  std::less<int>，此处可以训练用的lambda表达式
-            priority_queue<int, vector<int>, decltype(less)> q();
+            priority_queue<int, vector<int>, decltype(less)> queue(less);
+
+            for_each(stones.begin(), stones.end(), [&queue](int &each) { queue.push(each); });
+
+            while (queue.size() >= 2) {
+                int first_top = queue.top();
+                queue.pop();
+
+                int second_top = queue.top();
+                queue.pop();
+
+                if (first_top != second_top) {
+                    queue.push(abs(first_top - second_top));
+                }
+
+            }
+
+            return queue.empty() ? 0 : queue.top();
+        }
+
+
+        /**
+         * 尝试使用递归实现
+         *
+         * @param stones
+         * @return
+         */
+        int lastStoneWeight_v2(vector<int> &stones) {
+
 
         }
     };
