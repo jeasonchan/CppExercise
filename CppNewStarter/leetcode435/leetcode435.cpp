5602db6996fe88d5c64c5fe80b8dcab3b8f08689
jeasonchan <439994526@qq.com>
2021-01-01 18:02:40 +0800

    add leetcode 605 and 435

---
+++ CppNewStarter/leetcode435/leetcode435.cpp
@@ -0,0 +1,14 @@
+
+
+
+namespace leetcode435 {
+
+
+}
+
+
+int main(int argc, char *argv[]) {
+
+
+    return 0;
+}
---
+++ CppNewStarter/leetcode605/leetcode605.cpp
@@ -0,0 +1,41 @@
+#include <vector>
+
+
+namespace leetcode605 {
+    using namespace std;
+
+    class Solution {
+    public:
+        bool canPlaceFlowers(vector<int> &flowerbed, int n) {
+
+
+        }
+
+
+    private:
+        /**
+         * 检查当前位置是否能种花
+         *
+         * @param vector
+         * @param index
+         * @return
+         */
+        bool checkThenDone(vector<int> &vector, int &index) {
+            if (vector[index] != 0) {
+                return false;
+            }
+
+
+
+
+        }
+    };
+
+}
+
+
+int main(int argc, char *argv[]) {
+
+
+    return 0;
+}
