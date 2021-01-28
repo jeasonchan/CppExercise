2e621584506fe185ce08dc4b335fe776540036d0
jeasonchan <439994526@qq.com>
2021-01-28 18:26:41 +0800

    Create leetcode674.cpp

---
+++ CppNewStarter/leetcode674/leetcode674.cpp
@@ -0,0 +1,33 @@
+
+#include<vector>
+
+
+
+namespace leetcode674 {
+using namespace std;
+
+class Solution {
+public:
+    int findLengthOfLCIS(vector<int>& nums) {
+
+
+
+    }
+};
+
+
+}
+
+
+int main(int argc,char* argv[]){
+    std::vector<int> input{1,3,5,4,7};
+
+    leetcode674::Solution solution;
+
+    int&& ret=solution.findLengthOfLCIS(input);
+
+
+
+
+    return 0;
+}
