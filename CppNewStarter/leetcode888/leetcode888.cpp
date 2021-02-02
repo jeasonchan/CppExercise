5f050fff90d4281cbc70acbcea73eb75b136e79d
jeasonchan <439994526@qq.comgit>
2021-02-02 16:33:40 +0000

    update

---
+++ CppNewStarter/leetcode888/leetcode888.cpp
@@ -0,0 +1,54 @@
+/*
+ * @Author: your name
+ * @Date: 2021-02-02 16:16:47
+ * @LastEditTime: 2021-02-02 16:32:51
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /CppNewStarter/leetcode888/leetcode888.cpp
+ */
+#include "leetcode888.h"
+
+using namespace std;
+using namespace leetcode888_np;
+
+leetcode888::leetcode888()
+{
+    // TODO: Needs implementation
+}
+
+leetcode888::~leetcode888()
+{
+    // TODO: Needs implementation
+}
+
+leetcode888::leetcode888(const leetcode888& other)
+{
+    // TODO: Needs implementation
+}
+
+leetcode888::leetcode888(leetcode888&& other)
+{
+    // TODO: Needs implementation
+}
+
+leetcode888& leetcode888::operator=(const leetcode888 &other)
+{
+    // TODO: Needs implementation
+}
+
+leetcode888& leetcode888::operator=(leetcode888 &&other)
+{
+    // TODO: Needs implementation
+}
+
+
+
+
+
+
+int main(int argc,char* argv[]){
+
+    
+
+    return 0;
+}
---
+++ CppNewStarter/leetcode888/leetcode888.h
@@ -0,0 +1,25 @@
+#ifndef __LEETCODE888_H_A9L887AQW5K1__
+#define __LEETCODE888_H_A9L887AQW5K1__
+
+
+
+namespace leetcode888_np
+{
+    class leetcode888
+    {
+    public:
+        
+        leetcode888();
+        virtual ~leetcode888(); // TODO: Is VIRTUAL needed?
+        leetcode888(const leetcode888 &other);
+        leetcode888(leetcode888 &&other);
+        leetcode888 &operator=(const leetcode888 &other);
+        leetcode888 &operator=(leetcode888 &&other);
+
+    private:
+        // Members
+    };
+
+} // namespace leetcode888
+
+#endif // __LEETCODE888_H_A9L887AQW5K1__
