ad942f7a4e1bce4b7e6991bcd15cfa2de2c32290
jeasonchan <439994526@qq.comgit>
2021-02-19 17:01:01 +0000

    update

--- CppNewStarter/semaphore_mutex_condition/semaphore_mutex_condition.cpp
+++ CppNewStarter/semaphore_mutex_condition/semaphore_mutex_condition.cpp
@@ -1,14 +1,33 @@
-#include<search.h>
-#include<semaphore.h>
-#include<csignal>
+#include <semaphore.h>
+#include <mutex>
+#include <condition_variable>
+#include <iostream>
+#include <tuple>
+
+class Util
+{
+public:
+    static void printMainArgv(const int &argc, char *argv[])
+    {
+        for (int i = 0; i < argc; ++i)
+        {
+            std::cout << argv[i] << std::endl;
+        }
+    }
+};
 
 
+//也就是用返回值来初始化const int类型的临时量
+const int add(int a, int b)
+{
+    return a + b;
+}
 
+int main(int argc, char *argv[])
+{
+    Util::printMainArgv(argc, argv);
 
-
-int main(){
-
-
+    auto myTuplple = std::make_tuple(1, 2);
 
     return 0;
 }
