1963934e25033b3c2625680d0a6dbd1bb43ed70d
jeasonchan <439994526@qq.comgit>
2021-02-23 15:57:19 +0000

    update

---
+++ CppNewStarter/Linux多线程服务端编程实践/exe_1_1_3/main.cpp
@@ -0,0 +1,121 @@
+#include <mutex>
+#include <memory>
+#include <thread>
+#include <iostream>
+#include <chrono>
+
+// 匿名方法区，方法区内的符号天然对本编译单元内可见，且由于匿名性，可以等效认为对其他编译单元不可见
+namespace
+{
+    class unsafe_counter
+    {
+    private:
+        int times;
+
+    public:
+        unsafe_counter(/* args */) : times(0)
+        {
+        }
+
+        void increase()
+        {
+            ++(this->times);
+        }
+
+        int getTimes()
+        {
+            return this->times;
+        }
+    };
+
+    class safe_counter
+    {
+
+    private:
+        long times; //2147483647
+        std::mutex lock;
+
+    public:
+        safe_counter() : times(0) {}
+
+        void increase()
+        {
+            std::lock_guard<std::mutex> guard(this->lock);
+            ++(this->times);
+        }
+
+        int getTimes()
+        {
+            return this->times;
+        }
+    };
+
+}
+
+int main()
+{
+    using namespace std;
+
+    unsafe_counter counter01;
+    thread counter01_thread01([&counter01]() {
+        for (int i = 0; i < 10000000; ++i)
+        {
+            counter01.increase();
+        }
+    });
+
+    thread counter01_thread02([&counter01]() {
+        for (int i = 0; i < 10000000; ++i)
+        {
+            counter01.increase();
+        }
+    });
+
+    thread counter01_thread03([&counter01]() {
+        for (int i = 0; i < 10000000; ++i)
+        {
+            counter01.increase();
+        }
+    });
+
+    // 必须用detach，否则主线程会尝试析构线程，导致异常，这是CPP强制程序员有效进行资源管理
+    counter01_thread01.detach();
+    counter01_thread02.detach();
+    counter01_thread03.detach();
+
+    safe_counter counter02;
+    thread counter02_thread01([&counter02]() {
+        for (int i = 0; i < 10000000; ++i)
+        {
+            counter02.increase();
+        }
+    });
+
+    thread counter02_thread02([&counter02]() {
+        for (int i = 0; i < 10000000; ++i)
+        {
+            counter02.increase();
+        }
+    });
+
+    thread counter02_thread03([&counter02]() {
+        for (int i = 0; i < 10000000; ++i)
+        {
+            counter02.increase();
+        }
+    });
+    counter02_thread01.detach();
+    counter02_thread02.detach();
+    counter02_thread03.detach();
+
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+
+    cout << counter01.getTimes() << endl;
+    cout << counter02.getTimes() << endl;
+
+    void (safe_counter::*fun)() = &safe_counter::increase;
+    thread thread222(&safe_counter::increase, &counter02);
+    (counter02.*fun)();
+
+    return 0;
+}
